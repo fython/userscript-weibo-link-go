674ad2e065439bafb616b33a723a01aef0ba3594
Siubeng <fython@163.com>
2020-08-24 00:25:27 +0800

    Initial commit

---
+++ userscript.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name 渣浪微博外链自动跳转
+// @namespace https://github.com/fython
+// @version 0.1
+// @description 渣浪微博外链（t.cn）自动跳转
+// @author Siubeng (fython)
+// @match *://t.cn/*
+// @license  MIT
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function isValidUrl(string) {
+        try {
+            new URL(string);
+        } catch (_) {
+            return false;
+        }
+        return true;
+    }
+
+    const link = document.querySelector(".link").innerText;
+    if (isValidUrl(link)) {
+        window.location.href = link;
+    }
+})();
